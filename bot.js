56501787a489c44e3c4d64599f704b2c52a5fe59
EyadA7med2 <43893562+EyadA7med2@users.noreply.github.com>
2018-10-11 18:21:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = !
+const prefix = "!"
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`رجع البوت`,"http://twitch.tv/S-F")
-
+});
+client.on('ready', () => {
+    client.user.setGame(`رجعت البوت`, "http://twitch.tv/Streammingg")    
+});
 
 client.on('message',message =>{
     var prefix = "!";
@@ -50,50 +52,8 @@ client.on('message',message =>{
     }
     });
 
-
 client.on('message', message => {
-    var prefix = "!";
-   if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'clear')) {
-if(!message.channel.guild) return message.channel.send('**This Command is Just For Servers**').then(m => m.delete(5000));
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return      message.channel.send('**You Do not have permission** `MANAGE_MESSAGES`' );
-let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let request = `Requested By ${message.author.username}`;
-message.channel.send(`**Are You sure you want to clear the chat?**`).then(msg => {
-msg.react('✅')
-.then(() => msg.react('❌'))
-.then(() =>msg.react('✅'))
-
-let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-
-let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-reaction1.on("collect", r => {
-message.channel.send(`Chat will delete`).then(m => m.delete(5000));
-var msg;
-        msg = parseInt();
-
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-      message.channel.sendMessage("", {embed: {
-        title: "`` Chat Deleted ``",
-        color: 0x06DF00,
-        footer: {
-
-        }
-      }}).then(msg => {msg.delete(3000)});
-
-})
-reaction2.on("collect", r => {
-message.channel.send(`**Chat deletion cancelled**`).then(m => m.delete(5000));
-msg.delete();
-})
-})
-}
-});
-
-client.on('message', message => {
-    var prefix = "$"
+    var prefix = "!"
   if (message.author.x5bz) return;
   if (!message.content.startsWith(prefix)) return;
 
@@ -169,10 +129,37 @@ client.on('message', message => {
   })
 }
 });
-  
-  
-  
-  
-  
 
-client.login(process.env.BOT_TOKEN);
+client.on('message', message => {
+            if (message.content.startsWith(prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField('     **soon** ' ,' **soon** ')
+.addField('     **bc**  ' ,' **بث جماعي** ')
+.addField('     **soon** ' , '**soon**') 
+.addField('     **bc4** ' , '**soon**') 
+.addField('     **ping** ' ,' ** soon**')
+.addField('     **soon** ' , '**soon**')
+.addField('     **soon ** ' ,' **  soon  ** ')
+.addField('     **soon ** ' ,' **  soon  ** ')
+.addField('     **soon ** ' ,' **  soon  ** ')
+.setColor('#7d2dbe')
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('token'); 
