8199d40465fa30bd577c3c34908b1c3fd5ed6d01
EyadA7med2 <43893562+EyadA7med2@users.noreply.github.com>
2018-10-26 18:11:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 client.on('ready', () => {
-    client.user.setGame(`GL`, "http://twitch.tv/Streammingg")    
+    client.user.setGame(`!bc للبرودكاست`, "http://twitch.tv/Streammingg")    
 });
 
 client.on('message',message =>{
