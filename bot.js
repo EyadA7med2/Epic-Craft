c1f79c062b8dc7b1eda2311d4635088706c2294b
EyadA7med2 <43893562+EyadA7med2@users.noreply.github.com>
2018-10-27 19:00:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -193,31 +193,23 @@ client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name","✦ DISCORD MEMBER"));
     });
 
-client.on('message', message => {
-    var prefix = "!";
-     if(message.author.bot) return;
-
-    if (!message.content.startsWith(prefix)) return;
-    let command = message.content.split(" ")[0];
-    command = command.slice(prefix.length);
-    if (command == "warn") {
-
-        if (!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return;
-    let args = message.content.split(" ").slice(1);
-                    let reason = message.content.split(" ").slice(2).join(" ");
-                if (message.mentions.users.size < 1) return message.reply("**يرجى وضع منشن للشخص**").then(msg => msg.delete(3000));
-        if (!reason) return message.reply("**اكتب سبب التحذير**").then(msg => msg.delete(3000));
-        let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setTitle('New Warn !')
-        .addField(`${args}`)
-        message.channel.sendMessage(args.join("  "))
-        message.delete();
-
-
+client.on('message', msg => { 
+    if (msg.content.startsWith(`!$warn`)) {
+      if(!msg.member.hasPermission("MANAGE_MESSAGES")) return;
+       let args = msg.content.split(" ").slice(1);
+      if (!msg.mentions.members.first()) return msg.reply('منشن الشخص المحدد')
+      if (!args[0]) return msg.reply('اكتب السبب')
+      //غير اسم الروم او سوي روم بذا الاسم 
+      if (msg.guild.channels.find('name', 'warns')) {
+        //اذا غيرت فوق غير هنا كمان 
+        msg.guild.channels.find('name', 'warns').send(`
+      تم اعطائك انذار : ${msg.mentions.members.first()}
+      لأنك قمت بما يلي
+      ${args.join(" ").split(msg.mentions.members.first()).slice(' ')}
+      `)
+      }
     }
-
-});
+})
 
 
 
