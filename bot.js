a80ef7b3e462ea988f7b0e373f6ef9b10d978495
EyadA7med2 <43893562+EyadA7med2@users.noreply.github.com>
2018-12-18 19:16:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -335,6 +335,43 @@ var prefix = "!"
  
   })
 
+client.on('message', message => {
+  if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "kick") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**انت لا تمتلك صلاحيه KICK_MEMBERS**");
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**انا لا امتلك صلاحيه KICK_MEMBERS**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+  /*let b5bzlog = client.channels.find("name", "5bz-log");
+  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الطرد**");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+
+  message.guild.member(user).kick(7, user);
+
+  const banembed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setTitle('**Noobbot**')
+        .setDescription(`**Done Kick ${user}
+By ${message.author.username}** `)
+        .setAuthor(message.guild.name)
+  message.channel.send({
+    embed : banembed
+  })
+}
+});
+
 
 
 
