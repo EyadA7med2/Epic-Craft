e89b0ca1ef5b8fc438920bb1d786adeb6987d39b
EyadA7med2 <43893562+EyadA7med2@users.noreply.github.com>
2018-12-26 21:51:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -92,43 +92,93 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-    var prefix = "!"
-  if (message.author.x5bz) return;
-  if (!message.content.startsWith(prefix)) return;
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
+ var ms = require('ms')
 
-  let args = message.content.split(" ").slice(1);
+ var moment = require('moment');
 
-  if (command == "ban") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
+ var prefix = "!"
+  
+ if (message.author.x5bz) return;
+
+ if (!message.content.startsWith(prefix)) return;
+
+ let command = message.content.split(" ")[0];
+
+ command = command.slice(prefix.length);
+
+ let args = message.content.split(" ").slice(1);
+
+ let messageArray = message.content.split(" ");
+
+ let embed = new Discord.RichEmbed()
+
+.setImage("https://d.top4top.net/p_10880lo1r1.png")
+
+ if (command == "ban") {
+
+ if(!message.channel.guild) return message.reply('** This command only for servers**');
          
-  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
-  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-  /*let b5bzlog = client.channels.find("name", "5bz-log");
-  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if(!reason) return message.reply ("**اكتب سبب الطرد**");
-  if (!message.guild.member(user)
-  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+ if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**:x: You Don't Have ` BAN_MEMBERS ` Permission**");
+
+ if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**:x: I Don't Have ` BAN_MEMBERS ` Permission**");
+
+ let user = message.mentions.users.first();
+
+ let Reason = message.content.split(" ").slice(3).join(" ");
+
+ let time = messageArray[2];
+
+ if (message.mentions.users.size < 1) return message.channel.sendEmbed(embed)
+  
+ if (!message.guild.member(user).bannable) return message.reply("**:x:I Don't Have Permission For Ban This User**");
+
+ if(!time.match(/[1-60][s,m,h,d,w]/g))  return message.channel.send(':x: This Time Is Incorrect')
+
+ if(!Reason)  {
+
+ message.guild.member(user).ban({reason: Reason})
+
+ }
+
+  if(!Reason && time) {
 
   message.guild.member(user).ban(7, user);
 
-  const banembed = new Discord.RichEmbed()
-  .setAuthor(`BANNED!`, user.displayAvatarURL)
-  .setColor("RANDOM")
-  .setTimestamp()
-  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-  message.channel.send({
-    embed : banembed
-  })
-}
-});
+  }  
+
+  if(!time) {
+
+  message.guild.member(user).ban(7, user);
+
+  }
+  if(time) {
+  
+  setTimeout(() => {
+
+  message.guild.unban(user);
+
+  }, ms(time));
+
+  }
+
+  if(time && Reason && user) {
+
+  message.guild.member(user).ban({reason: Reason})
+
+  setTimeout(() => {
+
+  message.guild.unban(user);
+  
+  }, ms(time));
+
+  }
+
+  message.channel.send(`:white_check_mark:  ${user.tag} banned from the server ! :airplane:`)
+
+  }
+
+  });
 
 client.on('message', message => {
             if (message.content.startsWith(prefix + "help")) {
@@ -194,7 +244,7 @@ client.on('guildMemberAdd', member=> {
     });
 
 client.on('message', msg => { 
-    if (msg.content.startsWith(`!$warn`)) {
+    if (msg.content.startsWith(`!warn`)) {
       if(!msg.member.hasPermission("MANAGE_MESSAGES")) return;
        let args = msg.content.split(" ").slice(1);
       if (!msg.mentions.members.first()) return msg.reply('منشن الشخص المحدد')
@@ -336,41 +386,97 @@ var prefix = "!"
   })
 
 client.on('message', message => {
+
+  var ms = require('ms')
+ 
+  var moment = require('moment');
+ 
+  var prefix = "!"
+   
   if (message.author.x5bz) return;
+ 
   if (!message.content.startsWith(prefix)) return;
-
+ 
   let command = message.content.split(" ")[0];
+ 
   command = command.slice(prefix.length);
-
+ 
   let args = message.content.split(" ").slice(1);
+ 
+  let messageArray = message.content.split(" ");
+ 
+  let muteRole = message.guild.roles.find("name", "Muted");
 
-  if (command == "kick") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-         
-  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**انت لا تمتلك صلاحيه KICK_MEMBERS**");
-  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**انا لا امتلك صلاحيه KICK_MEMBERS**");
+  let embed = new Discord.RichEmbed()
+ 
+ .setImage("https://c.top4top.net/p_108825enp1.png")
+ 
+  if (command == "mute") {
+    
+  if(!muteRole) return message.guild.createRole({ name: "Muted", permissions: [] });
+
+  if(!message.channel.guild) return message.reply('** This command only for servers**');
+          
+  if(!message.guild.member(message.author).hasPermission("MUTE_MEMBERS")) return message.reply("**:x: You Don't Have ` MUTE_MEMBERS ` Permission**");
+ 
+  if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return message.reply("**:x: I Don't Have ` MUTE_MEMBERS ` Permission**");
+ 
   let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-  /*let b5bzlog = client.channels.find("name", "5bz-log");
-  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");*/
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if(!reason) return message.reply ("**اكتب سبب الطرد**");
-  if (!message.guild.member(user)
-  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+ 
+  let Reason = message.content.split(" ").slice(4).join(" ");
+ 
+  let time = messageArray[2];
+ 
+  if (message.mentions.users.size < 1) return message.channel.sendEmbed(embed)
+   
+  if (!message.guild.member(user).bannable) return message.reply("**:x:I Don't Have Permission For Mute This User**");
+ 
+  if(!Reason)  {
+ 
+    message.guild.member(user).addRole(muteRole);
+ 
+  }
+ 
+   if(!Reason && time) {
+ 
+    message.guild.member(user).addRole(muteRole);
+ 
+   }  
+ 
+   if(!time) {
+ 
+    message.guild.member(user).addRole(muteRole);
+ 
+   }
+   if(time) {
+    if(!time.match(/[1-60][s,m,h,d,w]/g))  return message.channel.send(':x: This Time Is Incorrect')
 
-  message.guild.member(user).kick(7, user);
+   setTimeout(() => {
+ 
+    message.guild.member(user).removeRole(muteRole);
+ 
+   }, ms(time));
+ 
+   }
+ 
+   if(time && Reason && user) {
+ 
+    message.guild.member(user).addRole(muteRole);
+ 
+   setTimeout(() => {
+ 
+    message.guild.member(user).removeRole(muteRole);
+   
+   }, ms(time));
+ 
+   }
+ 
+   message.channel.send(`:white_check_mark: ${user} has been muted ! :zipper_mouth:`)
+ 
+   }
+ 
+   });
 
-  const banembed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setTitle('**Noobbot**')
-        .setDescription(`**Done Kick ${user}
-By ${message.author.username}** `)
-        .setAuthor(message.guild.name)
-  message.channel.send({
-    embed : banembed
-  })
-}
-});
 
 client.on('message', message => {   
 if (message.author.boss) return;
