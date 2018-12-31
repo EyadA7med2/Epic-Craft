78e096d4dafdbb659f402804c2d2b15b4eaf46be
EyadA7med2 <43893562+EyadA7med2@users.noreply.github.com>
2018-12-31 22:54:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "$"
+const prefix = "$";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -30,7 +30,7 @@ client.on('ready', () => {
 
 client.on('ready', () => {
 
-    client.user.setActivity("$help | B3THRH SERVER | 2.K Soon",{type: 'Streaming'})
+    client.user.setActivity("$help | Beta V.0.0.1",{type: 'Streaming'})
 
 });
 
@@ -43,6 +43,51 @@ client.on("guildMemberAdd", member => {
 انت العضو رقم ${member.guild.memberCount} `) 
 }).catch(console.error)
 })
+
+client.on('message', message => {
+
+    var prefix = "$";
+          if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + 'bc')) {
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let copy = "SkyBot.";
+let request = `Requested By ${message.author.username}`;
+if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))
+.then(() =>msg.react('✅'))
+
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+message.channel.send(`**☑ | Done ... The Broadcast Message Has Been Sent For __${message.guild.members.size}__ Members**`).then(m => m.delete(5000));
+message.guild.members.forEach(m => {
+
+var bc = new
+   Discord.RichEmbed()
+   .setColor('RANDOM')
+   .setTitle('Broadcast')
+   .addField('سيرفر', message.guild.name)
+   .addField('المرسل', message.author.username)
+   .addField('الرسالة', args)
+   .setThumbnail(message.author.avatarURL)
+   .setFooter(copy, client.user.avatarURL);
+m.send({ embed: bc })
+msg.delete();
+})
+})
+reaction2.on("collect", r => {
+message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+msg.delete();
+})
+})
+}
+});
 
 client.on("message", message => {
     var prefix = "$";// البرفكس
@@ -1590,6 +1635,7 @@ if(message.content == prefix+"عواصم"){
                                       .setDescription(`
                                       __**Administrative Commands**__
                                     **  『$move @user / لسحب الشخص الى روومك』
+                                      『$bc / رسالة جماعية الى كل اعضاء السيرفر』
                                       『$hchannel / اخفاء الشات』
                                       『$schannel / اضهار الشات المخفية』
                                       『$clear / مسح الشات』
