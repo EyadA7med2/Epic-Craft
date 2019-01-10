8cf94dd66badadbee2c013fe53eab1a49e048a41
EyadA7med2 <43893562+EyadA7med2@users.noreply.github.com>
2019-01-10 18:49:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,13 +36,9 @@ client.on('ready', () => {
 
 
 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
-:crown:اسم العضو  ${member}:crown:  
-انت العضو رقم ${member.guild.memberCount} `) 
-}).catch(console.error)
-})
+
+
+
 
 client.on('message', message => {
 
